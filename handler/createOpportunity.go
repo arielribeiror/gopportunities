9b540c1c6a32e2a64eecb686944ddb070c245ace
Ariel Ribeiro <ariiel.ribeiro@gmail.com>
2023-08-14 20:31:18 -0400

    feat: add createOpportunity handler

--- handler/createOpportunity.go
+++ handler/createOpportunity.go
@@ -3,11 +3,36 @@ package handler
 import (
 	"net/http"
 
+	"github.com/arielribeiror/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpportunityHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST opportunity",
-	})
+	request := CreateOpportunityRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opportunity := schemas.Opportunity{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+		Range:    request.Range,
+	}
+
+	if err := db.Create(&opportunity).Error; err != nil {
+		logger.Errorf("error creating opportunity: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating opportunity on database")
+		return
+	}
+
+	sendSuccess(ctx, "create-opportunity", opportunity)
 }

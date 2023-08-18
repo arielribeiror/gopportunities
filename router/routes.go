42cfbfaecbf34623e6858d7e93564ac3f6b2d31a
Ariel Ribeiro <ariiel.ribeiro@gmail.com>
2023-08-17 20:00:11 -0400

    feat: add swaggo doc

--- router/routes.go
+++ router/routes.go
@@ -3,12 +3,20 @@ package router
 import (
 	"github.com/arielribeiror/gopportunities/handler"
 	"github.com/gin-gonic/gin"
+
+	docs "github.com/arielribeiror/gopportunities/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// Initialize Handler
 	handler.InitializeHandler()
-	v1 := router.Group("/api/v1")
+
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/opportunity", handler.GetOpportunityHandler)
 		v1.POST("/opportunity", handler.CreateOpportunityHandler)
@@ -16,4 +24,7 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opportunity", handler.UpdateOpportunityHandler)
 		v1.GET("/opportunities", handler.GetAllOpportunityHandler)
 	}
+
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
